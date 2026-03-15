91931de551806ff0c3909f846d8b5007c0013fef
kelaiya <kelaiyarao1@gmail.com>
2026-03-15 11:20:03 -0700

    Added classes

--- bankingSystem/bankingSystem.js
+++ bankingSystem/bankingSystem.js
@@ -1,3 +1,47 @@
+class Account {
+    constructor(accountId){
+        this.id = accountId;
+        this.balance = 0;
+        this.activity = 0;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+        this.activity += amount;
+        return this.balance;
+    }
+
+    withdraw(amount) {
+        if(this.balance < amount) return null;
+        this.balance -= amount;
+        this.activity += amount;
+        return this.balance;
+    }
+
+    merge(otherAccount) {
+        this.balance += otherAccount.balance;
+        this.activity += otherAccount.activity;
+    }
+}
+
+class Transfer {
+    constructor(transferId, fromAccount, toAccount, amount, timestamp){
+        this.id = transferId;
+        this.from = fromAccount;
+        this.to = toAccount;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+}
+
+class Cashback {
+    constructor(account, amount, readyTime) {
+        this.account = account;
+        this.amount = amount;
+        this.readyTime = readyTime;
+    }
+}
+
 class BankingSystem {
     constructor(){
         this.accounts = new Map();
@@ -7,82 +51,62 @@ class BankingSystem {
     }
 
     createAccount(accountId){
-        if(this.accounts.has(accountId)) return false
-        this.accounts.set(accountId, {balance: 0, activity: 0});
-        return true
+        if(this.accounts.has(accountId)) return "false";
+        this.accounts.set(accountId, new Account(accountId));
+        return "true";
     }
 
     deposit(accountId, amount){
-        if(!this.accounts.has(accountId)) return "";
         let account = this.accounts.get(accountId);
-        account.balance += amount;
-        account.activity += amount;
-        return String(account.balance);
+        if(!account) return "";
+        return String(account.deposit(amount));
     }
 
     withdraw(accountId, amount, timestamp){
-        if(!this.accounts.has(accountId)) return "";
         let account = this.accounts.get(accountId);
-        if(account.balance < amount) return "";
-        account.balance -= amount;
-        account.activity += amount;
+        if(!account) return "";
+        const newBalance = account.withdraw(amount);
+        if(newBalance == null) return ""
         const cashbackAmount = Math.floor(amount * 0.02);
-        this.cashbacks.push({
-            accountId: accountId,
-            amount: cashbackAmount,
-            time: timestamp + 10
-        })
-        return String(account.balance);
+        this.cashbacks.push(new Cashback(account, cashbackAmount, timestamp));
+        return String(newBalance);
     }
 
     topActivity(n){
-        const accountsArray = Array.from(this.accounts.entries());
-        accountsArray.sort((a, b) => {
-            if(b[1].activity != a[1].activity) return b[1].activity - a[1].activity;
-            return a[0].localCompare(b[0])
-        });
-        const result = [];
-        for(let i = 0; i < Math.min(n, accountsArray.length); i++){
-            const accountId = accountsArray[i][0];
-            const activity = accountsArray[i][1].activity;
-            result.push(`${accountId} (${activity})`);
-        }
-        return result.join(", ")
+        const sorted = Array.from(this.accounts.values()).sort((a, b) => b.activity - a.activity);
+        return sorted.slice(0, n).map(a => `${a.id}(${a.activity})`).join(", ");
     }
 
     transfer(fromId, toId, amount){
-        if(!this.accounts.has(fromId) || !this.accounts.has(toId)) return "";
         const fromAccount = this.accounts.get(fromId);
+        const toAccount = this.accounts.get(toId);
+        if(!fromAccount || !toAccount || fromId == toId) return "";
         if(fromAccount.balance < amount) return "";
-        fromAccount.balance -= amount;
-        fromAccount.activity += amount;
-        const transferId = "transfer" + this.nextTransferId;
-        this.nextTransferId++;
-        this.transfers.set(transferId, {from: fromId, to: toId, amount: amount});
+        const transferId = "transfer" + this.nextTransferId++;
+        const transfer = new Transfer(transferId, fromAccount, toAccount, amount);
+        this.transfers.set(transferId, transfer);
         return transferId;
     }
 
     acceptTransfer(accountId, transferId){
-        if(!this.transfers.has(transferId)) return "";
         const transfer = this.transfers.get(transferId);
-        if(transfer.to !== accountId) return "false";
-        const receiver = this.accounts.get(accountId);
-        receiver.balance += transfer.amount;
-        receiver.activity += transfer.amount;
+        if(!transfer || transfer.to !== accountId) return "false";
+        const fromAccount = transfer.from;
+        const toAccount = transfer.to;
+        fromAccount.withdraw(transfer.amount);
+        toAccount.deposit(transfer.amount);
         this.transfers.delete(transferId);
         return "true";
     }
 
     mergeAccounts(accountId1, accountId2){
-        if(!this.accounts.has(accountId1) || !this.accounts.has(accountId2)) return "false";
-        if(accountId1 == accountId2) return "false";
+        if(!this.accounts.has(accountId1) || !this.accounts.has(accountId2) || accountId1 == accountId2) return "false";
         const account1 = this.accounts.get(accountId1);
         const account2 = this.accounts.get(accountId2);
-        account1.balance += account2.balance;
-        account1.activity += account2.activity;
+        account1.merge(account2);
         for(const [transferId, transfer] of this.transfers.entries()){
-            if(transfer.from == accountId2) transfer.from = accountId1;
-            if(transfer.to == accountId2) transfer.to = accountId1;
+            if(transfer.from.id == accountId2) transfer.from = account1;
+            if(transfer.to.id == accountId2) transfer.to = account1;
         }
         this.accounts.delete(accountId2);
         return "true";
@@ -91,17 +115,14 @@ class BankingSystem {
     processCashbacks(currentTime){
         const remainingCashbacks = [];
         for(const cashback of this.cashbacks) {
-            if(cashback.time <= currentTime) {
-                const account = this.accounts.get(cashback.accountId);
-                if(account){
-                    account.balance += cashback.amount;
-                    account.activity += cashback.amount;
-                } else {
+            if(cashback.readyTime <= currentTime) {
+                cashback.account.balance += cashback.amount;
+                cashback.account.activity += cashback.amount;
+            } else {
                     remainingCashbacks.push(cashback);
-                }
             }
         }
-        this.cashbacks = remainingCashbacks
+        this.cashbacks = remainingCashbacks;
     }
 }
 
@@ -111,7 +132,7 @@ function solution(queries){
 
     for(const q of queries){
         const operation = q[0];
-        const timestamp = q[1];
+        const timestamp = Number(q[1]);
         bank.processCashbacks(timestamp);
         if(operation == "CREATE_ACCOUNT") result.push(bank.createAccount(q[2]));
         else if(operation == "DEPOSIT") result.push(bank.deposit(q[2], Number(q[3])));
@@ -125,10 +146,18 @@ function solution(queries){
 }
 
 const queries = [
-  ["CREATE_ACCOUNT","1","A1"],
-  ["DEPOSIT","2","A1","500"],
-  ["PAY","3","A1","200"],
-  ["TOP_ACTIVITY","86400010","1"]
+    ["DEPOSIT", "1", "A1", "100"],    
+    ["WITHDRAW", "2", "A1", "50"],   
+    ["CREATE_ACCOUNT","1","A1"],
+    ["CREATE_ACCOUNT","2","A1"],
+    ["TRANSFER", "2", "A1", "A2", "50"],
+    ["DEPOSIT","2","A1","500"],
+    ["WITHDRAW","3","A1","800"],
+    ["CREATE_ACCOUNT","2","A2"],
+    ["TOP_ACTIVITY","86400015","1"],
+    ["CREATE_ACCOUNT", "3", "A3"],
+    ["ACCEPT_TRANSFER", "6", "A3", "transfer1"],
+    ["ACCEPT_TRANSFER", "6", "A2", "transfer1"]
 ];
 
 console.log(solution(queries));
